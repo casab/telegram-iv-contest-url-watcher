38884514672784dbf18ef57b0cc0e348650f1585
casab <enginal@gmail.com>
2017-05-30 18:19:31 +0300

    Added persistance for repeating jobs

--- tracker.py
+++ tracker.py
@@ -1,9 +1,8 @@
-import requests, sys, logging, shelve
+import requests, sys, logging, shelve, os
 from telegram.ext import Updater, CommandHandler, Job
 from bs4 import BeautifulSoup
 from collections import defaultdict
 from persist import PersistentDict
-import os
 
 if sys.version_info.major == 3:
     from urllib.parse import urlparse
@@ -24,21 +23,6 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
-
-def start(bot, update):
-    update.message.reply_text(
-"""
-To watch a specific website, type;
-  /add url
-To remove a url from the watchlist, type;
-  /remove url
-To get the list of all watched urls, type:
-  /list
-To start monitoring type:
-  /monitor
-To stop monitoring type:
-  /stop
-""")
 
 def get_url_list(chat_id):
     chat_id = str(chat_id)
@@ -66,6 +50,21 @@ def remove_from_url_list(url, chat_id):
         temp.remove(url)
         d[chat_id] = temp
 
+def get_jobs_list():
+    with PersistentDict(JOB_LIST_FILE, 'c', format='json') as d:
+        jobs_list = d
+    return jobs_list
+
+def add_to_jobs_list(chat_id):
+    chat_id = str(chat_id)
+    with PersistentDict(JOB_LIST_FILE, 'c', format='json') as d:
+        d[chat_id] = True
+
+def remove_from_jobs_list(chat_id):
+    chat_id = str(chat_id)
+    with PersistentDict(JOB_LIST_FILE, 'c', format='json') as d:
+        d[chat_id] = False
+
 def url_formatter(url):
     parsed = urlparse(url)
     if (parsed.path):
@@ -76,6 +75,48 @@ def url_formatter(url):
         if parsed.netloc.startswith('www.'): return parsed.netloc[4:]
         return parsed.netloc
     else: return False
+
+def check_urls(bot, job):
+    r = requests.get('https://instantview.telegram.org/contest')
+    page = BeautifulSoup(r.text, 'html5lib')
+    url_list = get_url_list(job.context)
+    for url in url_list:
+        result = page.find('div', attrs={
+            'data-domain': url
+        })
+        if(result):
+            bot.send_message(
+                chat_id=job.context,
+                text="{} is added to contest page".format(url)
+            )
+            remove_from_url_list(url, job.context)
+
+def persistent_jobs(job_queue, old_jobs):
+    for chat_id, cond in old_jobs.items():
+        if cond:
+            jobs[chat_id].append(
+                job_queue.run_repeating(
+                    check_urls,
+                    interval=CHECK_PERIOD,
+                    context=chat_id
+                )
+            )
+
+def start(bot, update):
+    update.message.reply_text(
+"""
+To watch a specific website, type;
+  /add url
+To remove a url from the watchlist, type;
+  /remove url
+To get the list of all watched urls, type:
+  /list
+To start monitoring type:
+  /monitor
+To stop monitoring type:
+  /stop
+""")
+
 
 def add(bot, update, args):
     url = url_formatter(args[0])
@@ -99,38 +140,28 @@ def get_watchlist(bot, update):
     url_list = get_url_list(update.message.chat_id)
     update.message.reply_text("\n".join(url_list))
 
-def check_urls(bot, job):
-    r = requests.get('https://instantview.telegram.org/contest')
-    page = BeautifulSoup(r.text, 'html5lib')
-    url_list = get_url_list(job.context)
-    for url in url_list:
-        result = page.find('div', attrs={
-            'data-domain': url
-        })
-        if(result):
-            bot.send_message(
-                chat_id=job.context,
-                text="{} is added to contest page".format(url)
-            )
-            remove_from_url_list(url, job.context)
-
 def cb_watch(bot, update, job_queue):
+    chat_id = str(update.message.chat_id)
     if not jobs[update.message.chat_id]:
         job_watch = job_queue.run_repeating(
             check_urls,
             interval=CHECK_PERIOD,
             context=update.message.chat_id
         )
-        jobs[update.message.chat_id].append(job_watch)
+        jobs[chat_id].append(job_watch)
+        add_to_jobs_list(chat_id)
         update.message.reply_text("Started monitoring, type /stop to stop monitoring")
     else:
         update.message.reply_text("Already monitoring")
 
 def stop_watch(bot, update):
-    if jobs[update.message.chat_id]:
-        jobs[update.message.chat_id][0].schedule_removal()
-        del jobs[update.message.chat_id][0]
+    chat_id = str(update.message.chat_id)
+    if chat_id in jobs.keys():
+        jobs[chat_id][0].schedule_removal()
+        del jobs[chat_id][0]
+        remove_from_jobs_list(chat_id)
         update.message.reply_text("Stopped monitoring")
+
 
 updater = Updater(TELEGRAM_API_TOKEN)
 
@@ -144,4 +175,7 @@ updater.dispatcher.add_handler(CommandHandler('stop', stop_watch))
 
 
 updater.start_polling()
+
+persistent_jobs(updater.job_queue, get_jobs_list())
+
 updater.idle()
